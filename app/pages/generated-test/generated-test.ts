9585e955aac07397ad9cc1d0cd41a5da76ed0cf3
romulovp <romulocgd@hotmail.com>
2016-09-22 17:51:50 -0300

    Gerando Provider automaticamente e adicionado api de busca de ceo

--- app/pages/generated-test/generated-test.ts
+++ app/pages/generated-test/generated-test.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ConnectionService } from '../../providers/connection-service/connection-service';
 
 @Component({
   templateUrl: 'build/pages/generated-test/generated-test.html',
 })
 export class GeneratedTestPage {
 
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private connectionService: ConnectionService) {
 
+  }
+
+  buscarCep() : void {
+    this.connectionService.getCep('58434650')
+      .then((res) => {
+        let json = res.json();
+        console.log(json.localidade)
+      }).catch((err) =>{
+        console.log(err);
+      });
   }
 
 }
